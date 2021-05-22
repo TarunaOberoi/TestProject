ccf46ea003951e70c7e7249d28aedce84c390ca4
TarunaOberoi <84634702+TarunaOberoi@users.noreply.github.com>
2021-05-22 22:59:24 +0530

    fix code issue

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -6,9 +6,9 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-	Functions obj = New Functions();
-	int result = obj.add(10,12);
-            Console.WriteLine("Add of Two Number Is - " + int);
+            Functions obj = new Functions();
+	        int result = obj.add(10,12);
+            Console.WriteLine("Add of Two Number Is - " + result);
             Console.ReadLine();
         }
     }
