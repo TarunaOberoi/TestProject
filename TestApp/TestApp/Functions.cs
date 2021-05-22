3097ff20a32b3b54b23259512903ba4a4e6f9f28
TarunaOberoi <84634702+TarunaOberoi@users.noreply.github.com>
2021-05-22 22:21:24 +0530

    sencond Commit

---
+++ TestApp/TestApp/Functions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestApp
+{
+    public class Functions
+    {
+        public int add(int firstInput, int secondInput)
+        {
+            return (firstInput + secondInput);
+        }
+
+    }
+}
